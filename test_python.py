c0a97d8317d74d635233b3e5b003ca3a764cc08b
glrh11 <glrh11@glrh11deMacBook-Pro.local>
2019-06-25 17:52:53 +0800

    release B;

--- test_python.py
+++ test_python.py
@@ -9,3 +9,19 @@ sentry_sdk.init(
 
 division_by_zero = 1 / 0
 
+
+"""
+export SENTRY_ORG=zzu
+# new release A
+sentry-cli releases new -p python A
+
+# notify commits
+sentry-cli releases set-commits --commit "glrh111/read_in_life_weapp@421c4efd36fb4c1a7d9016b35840795bac1e4f65..0b3fc9463d9de9d77ba3af834384b564f3467571" A
+
+# new release B 对于上述代码，不执行这条命令也行。因为代码里边关联了 release，上报 event 时会自动新建 release
+sentry-cli releases new -p python B
+
+sentry-cli releases set-commits --commit "glrh111/read_in_life_weapp@.." A
+
+"""
+
